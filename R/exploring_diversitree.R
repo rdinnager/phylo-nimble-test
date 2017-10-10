b46ccb3b158eec877fb9a0bb2be9be199734cf36
rdinnager <r.dinnage@gmail.com>
2017-10-10 22:51:11 +1100

    added get.ordering function

--- R/exploring_diversitree.R
+++ R/exploring_diversitree.R
@@ -1,4 +1,17 @@
 library(nimble)
+library(diversitree)
+
+set.seed(9102017)
+
+pars <- c(.1,  .15,  .2,  # lambda 1, 2, 3
+          .03, .045, .06, # mu 1, 2, 3
+          .05, 0,         # q12, q13
+          .05, .05,       # q21, q23
+          0,   .05)       # q31, q32
+
+phy <- tree.musse(pars, max.taxa = 30, x0=1)
+plot(phy)
+phy$tip.state
 
 edges <- phy$edge
 n.tip <- length(phy$tip.label)
@@ -23,7 +36,7 @@ children_nimble <- nimbleFunction(
   }
 )
 
-test <- children_nimble(edges, n.tip)
+children <- children_nimble(edges, n.tip)
 test_comp <- compileNimble(children_nimble)
 test_comp(edges, n.tip)
 
@@ -33,3 +46,36 @@ root <- n.tip + 1
 is.tip <- idx <= n.tip
 
 diversitree:::get.ordering(test, is.tip, root)
+
+get_ordering_nimble <- nimbleFunction(
+  run = function(children = integer(2), n.tip = integer(0), root = integer(0)) {
+    n_nodes <- dim(children)[1] - n.tip
+    node_order <- integer(length = n_nodes, value = 0)
+    node_order[1] <- root
+    iter <- 2L
+    
+    i <- root
+    while(length(i) > 0) {
+      kids <- children[i, ]
+      i <- kids[kids > n.tip]
+      if(length(i) > 0) {
+        i_2 <- i
+        for(j in 1:length(i)) {
+          i_2[j] <- i[length(i) - j + 1]
+        }
+        node_order[iter:(iter + length(i) - 1)] <- i_2
+        iter <- iter + length(i)
+      }
+    }
+    node_order_rev <- node_order
+    for(j in 1:length(node_order)) {
+      node_order_rev[j] <- node_order[length(node_order) - j + 1]
+    }
+    returnType(integer(1))
+    return(node_order_rev)
+  }
+)
+
+test2 <- get_ordering_nimble(children, n.tip, root)
+test2
+
